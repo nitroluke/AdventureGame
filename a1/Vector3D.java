2029a6645f41c1b828f623f3c93de03561a1a65b
kalvyn.lu <kalvynlu@gmail.com>
2015-09-01 17:38:05 -0600

    negate, tabs to space

--- a1/Vector3D.java
+++ a1/Vector3D.java
@@ -49,9 +49,6 @@ public class Vector3D {
     public boolean equals(Vector3D v) {
         return dequals(this.x, v.x) && dequals(this.y, v.y) && dequals(this.z, v.z);
     }
-    
-
-<<<<<<< HEAD
 	/**
 	 * Scales a vector by a scalar
 	 * @param scale The scalar to scale the vector
@@ -64,14 +61,4 @@ public class Vector3D {
 	public Vector3D negate(){
 		return this.scale(-1);
 	}
-=======
-    /**
-     * Scales a vector by a scalar
-     * @param scale The scalar to scale the vector
-     * @return the scaled vector
-     */
-    public Vector3D scale(double scale){
-        return new Vector3D(scale*x, scale*y, scale*z);
-    }
->>>>>>> ce305a66c34c70543252ff7212bd11c04f7c2b1f
 }
