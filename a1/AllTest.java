5a1d51132e882e19141b0581068f567edb189d94
Todd Beckman <toddbeckman1@gmail.com>
2015-09-01 15:36:24 -0600

    prepared test cases for equal and Vector3D constructor

--- a1/AllTest.java
+++ a1/AllTest.java
@@ -6,9 +6,30 @@ import org.junit.Test;
 
 public class AllTest {
 
+	public void testEquals() {
+		//	Obvious true
+		Vector3D a = new Vector3D(1.0, 1.0, 1.0);
+		Vector3D b = new Vector3D(1.0, 1.0, 1.0);
+		assertTrue(a.equals(b));
+		//	Obvious false (test each component)
+		Vector3D c = new Vector3D(2.0, 1.0, 1.0);
+		Vector3D d = new Vector3D(2.0, 2.0, 1.0);
+		Vector3D e = new Vector3D(2.0, 1.0, 2.0);
+		assertFalse(a.equals(c));
+		assertFalse(a.equals(d));
+		assertFalse(a.equals(e));
+		//	False, because the tolerance states difference must be no greater than .001 
+		Vector3D f = new Vector3D(1.001, 1.0, 1.0);
+		Vector3D g = new Vector3D(0.999, 1.0, 1.0);
+		assertFalse(f.equals(g));
+		//	True, because the difference is less than 0.001
+		Vector3D h = new Vector3D(1.0005, 1.0, 1.0);
+		assertTrue(a.equals(h));
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		testEquals();
 	}
 
 }
--- a1/Vector3D.java
+++ a1/Vector3D.java
@@ -1,5 +1,14 @@
 package esof322.a1;
 
 public class Vector3D {
-
+	public static final double EQUAL_TOLERANCE = 0.001;
+	public final double x, y, z;
+	public Vector3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public boolean equals(Vector3D v) {
+		return false;
+	}
 }
