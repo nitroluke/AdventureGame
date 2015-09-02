eb3cbfd008e06448df30dca4923840b1e64fc3b7
Luke Welna <lwelna@sofi.org>
2015-09-01 18:02:15 -0600

    Fix code formatting and Kalvyn's inability to change tabs to spaces

--- a1/Vector3D.java
+++ a1/Vector3D.java
@@ -17,8 +17,10 @@ public class Vector3D {
      * The third component of the vector
      */
     public final double z;
+
     /**
      * Constructs a Vector3D from three components
+     *
      * @param x The first component of the vector
      * @param y The second component of the vector
      * @param z The third component of the vector
@@ -28,12 +30,15 @@ public class Vector3D {
         this.y = y;
         this.z = z;
     }
+
     @Override
     public String toString() {
         return "(" + x + ", " + y + ", " + z + ")";
     }
+
     /**
      * Determines if two doubles are similar enough to be equal using EQUAL_TOLERANCE
+     *
      * @param a The first double to compare
      * @param b The second double to compare
      * @return Whether the two doubles are equal
@@ -41,24 +46,28 @@ public class Vector3D {
     private boolean dequals(double a, double b) {
         return Math.abs(a - b) < EQUAL_TOLERANCE;
     }
+
     /**
      * Determines if two Vector3D's are similar enough to be equal using EQUAL_TOLERANCE
+     *
      * @param v The vector to compare with this one
      * @return Whether the two vectors are equal
      */
     public boolean equals(Vector3D v) {
         return dequals(this.x, v.x) && dequals(this.y, v.y) && dequals(this.z, v.z);
     }
+
     /**
      * Scales a vector by a scalar
+     *
      * @param scale The scalar to scale the vector
      * @return the scaled vector
      */
-    public Vector3D scale(double scale){
-   	    return new Vector3D(scale*x, scale*y, scale*z);
+    public Vector3D scale(double scale) {
+        return new Vector3D(scale * x, scale * y, scale * z);
     }
-    
-    public Vector3D negate(){
-    	return this.scale(-1);
-	}
+
+    public Vector3D negate() {
+        return this.scale(-1);
+    }
 }
